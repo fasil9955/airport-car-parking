0263e07a74294ed7616324761e49ca13153e4b4b
fasil9955 <fasilpvr52@gmail.com>
2023-03-02 15:42:12 +0530

    added footer ,header,searchform

--- car_parking/src/App.js
+++ car_parking/src/App.js
@@ -1,23 +1,17 @@
 import Home from './pages/home.js';
-import Header from './pages/header';
-import Footer from './pages/footer';
+
 
 
 
 const App = () => {
   return (
     <div>
-      <div>
-        <Header/>
-      </div>
+      
       <div>
         <Home/>
       </div>
       
-      <div>
-
-        <Footer/>
-      </div>
+      
      
        
       
