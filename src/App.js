e819e24229c04d868b1e47ca25c6a6986e2f208d
fasil9955 <fasilpvr52@gmail.com>
2023-03-14 18:41:21 +0530

    step 7,8

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import HomePage from './pages/HomePage';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Login from './pages/Login';
+import AirportAvailability from './pages/AirportAvailability'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="results" element={<AirportAvailability />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<HomePage />} />
+    </Route>
+  </Routes>
+  </BrowserRouter >
   );
 }
 
